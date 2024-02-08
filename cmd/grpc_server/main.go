81a4b290cf33e8d0f0a1d7630836b6fc58c41dee
Tauzhnianskyi Artem <tauzhnianskyi.artem@gmail.com>
2024-02-08 19:38:19 +0200

    Add api specification for protobuf

---
+++ cmd/grpc_server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	desc "github.com/TauzhnianskyiArtem/chat-service/pkg/chat_v1"
+)
+
+const grpcPort = 9092
+
+type server struct {
+	desc.UnimplementedChatV1Server
+}
+
+func (s *server) Create(ctx context.Context, req *desc.ChatCreateRequest) (*desc.ChatCreateResponse, error) {
+	log.Printf("Usernames: %v", req.GetUsernames())
+
+	return &desc.ChatCreateResponse{Id: gofakeit.Int64()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,15 @@ module github.com/TauzhnianskyiArtem/chat-service
 go 1.20
 
 require (
-	github.com/fatih/color v1.16.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/brianvoe/gofakeit v3.18.0+incompatible
+	github.com/golang/protobuf v1.5.3
+	google.golang.org/grpc v1.61.0
+	google.golang.org/protobuf v1.32.0
+)
+
+require (
+	golang.org/x/net v0.18.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
 )
